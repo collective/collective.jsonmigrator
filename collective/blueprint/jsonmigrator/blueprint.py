13c86d2c52b1da2b95f4859cca98f27024cd9d54
Rok Garbas <rok@garbas.si>
2010-06-17 12:45:53 +0200

    removed statistic blueprint by mistake, is back

--- collective/blueprint/jsonmigrator/blueprint.py
+++ collective/blueprint/jsonmigrator/blueprint.py
@@ -1,5 +1,8 @@
+
+import time
 import os
 import simplejson
+import logging
 
 from DateTime import DateTime
 from Acquisition import aq_base
@@ -19,6 +22,7 @@ from Products.CMFCore.utils import getToolByName
 from Products.Archetypes.interfaces import IBaseObject
 
 DATAFIELD = '_datafield_'
+STATISTICSFIELD = '_statistics_field_prefix_'
 
 
 class JSONSource(object):
@@ -61,6 +65,61 @@ class JSONSource(object):
                         item[key] = os.path.join(self.path, item[key])
 
                 yield item
+
+class Statistics(object):
+    """ This has to be placed in the pipeline just after all sources
+    """
+
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.stats = {'START_TIME':     int(time.time()),
+                      'TIME_LAST_STEP': 0,
+                      'STEP':           options.get('log-step', 25),
+                      'OBJ_COUNT':      0,
+                      'EXISTED':        0,
+                      'ADDED':          0,
+                      'NOT-ADDED':      0,}
+        self.pathkey = defaultMatcher(options, 'path-key', name, 'path')
+        self.statistics_prefix = options.get('statisticsfield-prefix', STATISTICSFIELD)
+        self.transmogrifier = transmogrifier
+        self.name = name
+        self.options = options
+        self.previous = previous
+        self.context = transmogrifier.context
+
+    def __iter__(self):
+        for item in self.previous:
+
+            self.stats['OBJ_COUNT'] += 1
+
+            yield item
+
+            if item[self.statistics_prefix + 'existed']:
+                self.stats['EXISTED'] += 1
+            else:
+                keys = item.keys()
+                pathkey = self.pathkey(*keys)[0]
+                path = item[pathkey]
+                path = path.encode('ASCII')
+                context = self.context.unrestrictedTraverse(path, None)
+                if context is not None and path == '/'.join(context.getPhysicalPath()):
+                    self.stats['ADDED'] += 1
+                else:
+                    self.stats['NOT-ADDED'] += 1
+
+            if self.stats['OBJ_COUNT'] % self.stats['STEP'] == 0:
+                now = int(time.time())
+                stat = 'COUNT: %d; ' % self.stats['OBJ_COUNT']
+                stat += 'TOTAL TIME: %d; ' % (now - self.stats['START_TIME'])
+                stat += 'STEP TIME: %d; ' % (now - self.stats['TIME_LAST_STEP'])
+                self.stats['TIME_LAST_STEP'] = now
+                stat += 'EXISTED: %d; ADDED: %d; NOT-ADDED: %d' % (
+                                               self.stats['EXISTED'],
+                                               self.stats['ADDED'],
+                                               self.stats['NOT-ADDED'])
+                logging.warning(stat)
 
 
 class Mimetype(object):
