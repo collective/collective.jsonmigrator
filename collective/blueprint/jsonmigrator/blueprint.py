1b9dc015b16f29d6cd8fcb7667bc628a7705fbc4
enriquepablo <enriquepablo@gmail.com>
2010-07-15 17:48:18 +0200

    local roles

--- collective/blueprint/jsonmigrator/blueprint.py
+++ collective/blueprint/jsonmigrator/blueprint.py
@@ -430,6 +430,49 @@ class PermissionMapping(object):
 
             yield item
 
+class LocalRoles(object):
+    """ """
+
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.transmogrifier = transmogrifier
+        self.name = name
+        self.options = options
+        self.previous = previous
+        self.context = transmogrifier.context
+
+        if 'path-key' in options:
+            pathkeys = options['path-key'].splitlines()
+        else:
+            pathkeys = defaultKeys(options['blueprint'], name, 'path')
+        self.pathkey = Matcher(*pathkeys)
+
+        if 'local-roles-key' in options:
+            roleskeys = options['local-roles-key'].splitlines()
+        else:
+            roleskeys = defaultKeys(options['blueprint'], name, 'ac_local_roles')
+        self.roleskey = Matcher(*roleskeys)
+
+    def __iter__(self):
+        for item in self.previous:
+            pathkey = self.pathkey(*item.keys())[0]
+            roleskey = self.roleskey(*item.keys())[0]
+
+            if not pathkey or not roleskey or \
+               roleskey not in item:    # not enough info
+                yield item; continue
+
+            obj = self.context.unrestrictedTraverse(item[pathkey].lstrip('/'), None)
+            if obj is None:             # path doesn't exist
+                yield item; continue
+
+            if IRoleManager.providedBy(obj):
+                obj.__ac_local_roles__ = item[roleskey]
+
+            yield item
+
 
 class DataFields(object):
     """ """
