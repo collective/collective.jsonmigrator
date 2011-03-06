747af7d0be1bf16f12822ef4841f40f5bb23a6b6
Rok Garbas <rok@garbas.si>
2011-03-06 20:01:50 +0100

    renaming to collective.jsonmigrator since its going to be more then just collection of blueprints

--- setup.py
+++ setup.py
@@ -1,32 +1,35 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
+from setuptools import find_packages
 
 version = '0.2'
 
-setup(name='collective.blueprint.jsonmigrator',
-      version=version,
-      description="useful transmogrifier blueprints for large migrations from Plone, from 2.0 to 4.0",
-      long_description=open("README.txt").read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='collective.jsonmigrator',
+    version=version,
+    description="Migrations from legacy Plone sites (2.0, 2.1, 2.5) to Plone 4.0",
+    long_description=open("README.txt").read(),
+    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective', 'collective.blueprint'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'simplejson',
-      ],
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    keywords='plone transmogrifier ',
+    author='Rok Garbas',
+    author_email='rok@garbas.si',
+    url='https://github.com/collective/',
+    license='BSD',
+    packages=find_packages(),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'simplejson',
+        'collective.transmogrifier',
+        'plone.app.transmogrifier',
+        ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
