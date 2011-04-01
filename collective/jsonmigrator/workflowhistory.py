a2b935d131758cfd8cd37be1375a5139e57b8675
Rok Garbas <rok@garbas.si>
2011-04-01 03:37:17 +0200

    adding WorkflowHistory blueprint

---
+++ collective/jsonmigrator/workflowhistory.py
@@ -0,0 +1,67 @@
+from zope.interface import implements
+from zope.interface import classProvides
+from collective.transmogrifier.interfaces import ISectionBlueprint
+from collective.transmogrifier.interfaces import ISection
+from collective.transmogrifier.utils import Matcher
+from collective.transmogrifier.utils import defaultKeys
+from Products.CMFCore.utils import getToolByName
+from Products.Archetypes.interfaces import IBaseObject
+from DateTime import DateTime
+
+
+class WorkflowHistory(object):
+    """
+    """
+
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.transmogrifier = transmogrifier
+        self.name = name
+        self.options = options
+        self.previous = previous
+        self.context = transmogrifier.context
+        self.wftool = getToolByName(self.context, 'portal_workflow')
+
+        if 'path-key' in options:
+            pathkeys = options['path-key'].splitlines()
+        else:
+            pathkeys = defaultKeys(options['blueprint'], name, 'path')
+        self.pathkey = Matcher(*pathkeys)
+
+        if 'workflowhistory-key' in options:
+            workflowhistorykeys = options['workflowhistory-key'].splitlines()
+        else:
+            workflowhistorykeys = defaultKeys(options['blueprint'], name, 'workflow_history')
+        self.workflowhistorykey = Matcher(*workflowhistorykeys)
+
+
+    def __iter__(self):
+        for item in self.previous:
+            pathkey = self.pathkey(*item.keys())[0]
+            workflowhistorykey = self.workflowhistorykey(*item.keys())[0]
+
+            if not pathkey or not workflowhistorykey or \
+               workflowhistorykey not in item:  # not enough info
+                yield item; continue
+
+            obj = self.context.unrestrictedTraverse(item[pathkey].lstrip('/'), None)
+            if obj is None or not getattr(obj, 'workflow_history', False):
+                yield item; continue
+
+            if IBaseObject.providedBy(obj):
+                item_tmp = item
+
+                # get back datetime stamp and set the workflow history
+                for workflow in item_tmp[workflowhistorykey]:
+                    for k, workflow2 in enumerate(item_tmp[workflowhistorykey][workflow]):
+                        item_tmp[workflowhistorykey][workflow][k]['time'] = DateTime(item_tmp[workflowhistorykey][workflow][k]['time'])
+                obj.workflow_history.data = item_tmp[workflowhistorykey]
+
+                # update security
+                workflows = self.wftool.getWorkflowsFor(obj)
+                if workflows:
+                    workflows[0].updateRoleMappingsFor(obj)
+
+            yield item
