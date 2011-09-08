3e4ac0ff3be98661d7a7671f07a47816de38e41b
Thomas Buchberger <t.buchberger@4teamwork.ch>
2011-09-08 12:02:53 +0200

    Made the jsonmigrator form work with the catalog source.

--- collective/jsonmigrator/helper.py
+++ collective/jsonmigrator/helper.py
@@ -5,9 +5,11 @@ from zope.schema import URI
 from zope.schema import Int
 from zope.schema import List
 from zope.schema import Choice
+from zope.schema import Text
 from zope.schema import TextLine
 from zope.schema import ASCIILine
 from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.interfaces import IFromUnicode, IList
 from z3c.form import form
 from z3c.form import field
 from z3c.form import button
@@ -16,11 +18,11 @@ from plone.z3cform.layout import wrap_form
 from collective.transmogrifier.transmogrifier import Transmogrifier
 from collective.transmogrifier.transmogrifier import configuration_registry
 from collective.transmogrifier.transmogrifier import _load_config
-from collective.jsonmigrator.remotesource import RemoteSource
 from collective.jsonmigrator import JSONMigratorMessageFactory as _
 from collective.jsonmigrator import logger
 
-
+SOURCE_SECTIONS = frozenset(['collective.jsonmigrator.remotesource',
+                             'collective.jsonmigrator.catalogsource'])
 
 class IJSONMigratorRun(Interface):
     """ remote source interface
@@ -67,6 +69,19 @@ class IJSONMigratorRun(Interface):
             required=True,
             )
 
+    catalog_path = TextLine(
+        title=_(u"Catalog Path"),
+        description=_(u"The absolute path of the catalog tool."),
+        required=True,
+    )
+
+    catalog_query = Text(
+        title=_(u"Catalog Query"),
+        description=_("Specify query parameters in dict notation. If left "
+                      "empty, all items will be returned."),
+        required=False,
+    )
+
 
 class JSONMigratorRun(form.Form):
 
@@ -79,22 +94,32 @@ class JSONMigratorRun(form.Form):
         section = None
         for section_id in config.keys():
             tmp = config[section_id]
-            if tmp.get('blueprint', '') == RemoteSource.name:
+            if tmp.get('blueprint', '') in SOURCE_SECTIONS:
                 section = tmp
                 break
         if not section:
-            raise Exception("Section with blueptint '%s' not found!" %
-                    RemoteSource.__name__)
-        for option, default in RemoteSource._options:
+            raise Exception("Source section not found.")
+
+        # Omit some fields depending on the selected source
+        if section['blueprint'] == 'collective.jsonmigrator.catalogsource':
+            self.fields = self.fields.omit('remote_path',
+                                           'remote_crawl_depth',
+                                           'remote_skip_path')
+        elif section['blueprint'] == 'collective.jsonmigrator.remotesource':
+            self.fields = self.fields.omit('catalog_path', 'catalog_query')
+
+        # Fill in default values from the transmogrifier config file
+        for option, value in section.items():
             field = self.fields.get(option.replace('-', '_'))
-            default = section.get(option, default)
-            if field.__name__ in ['remote_password', 'remote_path']:
-                default = unicode(default)
-            elif field.__name__ == 'remote_skip_path':
-                default = [unicode(i) for i in default.split()]
-            elif field.__name__ == 'remote_crawl_depth':
-                default = int(default)
-            field.field.default = default
+            if field:
+                field = field.field
+                value = value.decode('utf8')
+                if IFromUnicode.providedBy(field):
+                    field.default = field.fromUnicode(value)
+                elif IList.providedBy(field):
+                    field.default = [field.value_type.fromUnicode(v) for v
+                                     in value.split()]
+
         super(JSONMigratorRun, self).updateWidgets()
         self.widgets['config'].mode = interfaces.HIDDEN_MODE
 
@@ -109,6 +134,7 @@ class JSONMigratorRun(form.Form):
         logger.info("Stop importing profile: " + data['config'])
 
 
+
 class JSONMigratorConfigurations(object):
 
     def __call__(self, context):
@@ -117,7 +143,7 @@ class JSONMigratorConfigurations(object):
             conf_file = _load_config(conf_id)
             for section_id in conf_file.keys():
                 section = conf_file[section_id]
-                if section.get('blueprint', '') == RemoteSource.name:
+                if section.get('blueprint', '') in SOURCE_SECTIONS:
                     conf = configuration_registry.getConfiguration(conf_id)
                     terms.append(SimpleVocabulary.createTerm(
                             conf_id, conf_id, conf['title']))
