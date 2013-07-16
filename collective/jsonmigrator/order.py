cb196b28decb122bc4d1b7d24354de06fb0c9e5d
Elio Schmutz <elio.schmutz@4teamwork.ch>
2013-07-16 14:09:16 +0200

    Fix: remove not yet released traverse method from collective.transmogrifier

--- collective/jsonmigrator/order.py
+++ collective/jsonmigrator/order.py
@@ -3,7 +3,6 @@ from zope.interface import classProvides, implements
 from collective.transmogrifier.interfaces import ISectionBlueprint
 from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.utils import defaultMatcher
-from collective.transmogrifier.utils import traverse
 from zope.app.container.contained import notifyContainerModified
 
 
@@ -49,7 +48,11 @@ class OrderSection(object):
             for pos, key in enumerate(ordered_keys):
                 normalized_positions[key] = pos
 
-            parent = traverse(self.context, path)
+            # TODO: After the new collective.transmogrifier release (>1.4), the
+            # utils.py provides a traverse method.
+            # from collective.transmogrifier.utils import traverse
+            # parent = traverse(self.context, path)
+            parent = self.context.unrestrictedTraverse(path.lstrip('/'))
             if not parent:
                 continue
 
