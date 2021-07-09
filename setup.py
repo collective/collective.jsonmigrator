638a779968b2a9a84e7aefea8079dddcf90b0f35
Jens W. Klein <jk@kleinundpartner.at>
2021-07-09 18:27:05 +0200

    even isort setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """Installer for the collective.jsonmigrator package."""
 
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from setuptools import setup
 
 
 long_description = "\n\n".join(
