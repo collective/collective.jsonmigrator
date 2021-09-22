b3317cfde2a6a75821966f1dbda6205cfc082168
wesleybl <wesleybl@gmail.com>
2021-09-22 12:36:47 -0300

    Preparing release 2.0.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.0.2 (unreleased)
+2.0.0 (2021-09-22)
 ------------------
 
 - Add blueprint to import translations from plone.app.multilingual.
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.jsonmigrator",
-    version="1.0.2.dev0",
+    version="2.0.0",
     description="Tool for you to migrate from old Plone sites",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
